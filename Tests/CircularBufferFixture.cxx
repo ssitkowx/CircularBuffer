f525f3fe7195fc90b07fa8e015c87f247c882873
sylsit <sitkowskis@interia.pl>
2021-01-08 18:10:27 +0100

    Tests added.

---
+++ Tests/CircularBufferFixture.cxx
@@ -0,0 +1,88 @@
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// INCLUDES /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+#include "LoggerMock.h"
+#include "CircularBufferFixture.hxx"
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// FUNCTIONS ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+TEST_F (CircularBufferFixture, CheckTheConsistencyOfTheDataInTheBufferAfterItHasBeenOverwritten )
+{
+    LOGW (MODULE, "CheckTheConsistencyOfTheDataInTheBufferAfterItHasBeenOverwritten");
+
+    LOGD (MODULE, "Overwrite the data buffer");
+    for (uint8_t dataNum = ZERO; dataNum < 2 * CircBufferLen; dataNum++)
+    {
+        CircBuffer.Add (dataNum);
+    }
+
+    LOGD (MODULE, "Check the contents of the buffer");
+
+    for (uint8_t dataNum = ZERO; dataNum < CircBufferLen; dataNum++)
+    {
+        uint8_t expectedData = dataNum + TEN;
+        uint8_t foundData    = CircBuffer.Remove ();
+
+        ASSERT_EQ (expectedData, foundData);
+    }
+}
+
+TEST_F (CircularBufferFixture, CheckTheConsistencyOfTheDataInTheBufferAfterItHasBeenFilledAndFullyEmptied)
+{
+    LOGW (MODULE, "CheckTheConsistencyOfTheDataInTheBufferAfterItHasBeenFilledAndFullyEmptied");
+
+    LOGD (MODULE, "Fill the data buffer");
+    for (uint8_t dataNum = ZERO; dataNum < CircBufferLen; dataNum++)
+    {
+        CircBuffer.Add (dataNum);
+    }
+
+    LOGD (MODULE, "Empty the data buffer");
+    for (uint8_t dataNum = ZERO; dataNum < CircBufferLen; dataNum++)
+    {
+        CircBuffer.Remove ();
+    }
+
+    LOGD (MODULE, "Check the contents of the buffer");
+    for (uint8_t dataNum = ZERO; dataNum < CircBufferLen; dataNum++)
+    {
+        ASSERT_EQ (ZERO, CircBuffer.Remove());
+    }
+}
+
+TEST_F (CircularBufferFixture, CheckTheConsistencyOfTheDataInTheBufferAfterItHasBeenFilledAndHalfEmptied)
+{
+    LOGW (MODULE, "CheckTheConsistencyOfTheDataInTheBufferAfterItHasBeenFilledAndHalfEmptied");
+
+    LOGD (MODULE, "Fill the data buffer");
+    for (uint8_t dataNum = ZERO; dataNum < CircBufferLen; dataNum++)
+    {
+        CircBuffer.Add (dataNum);
+    }
+
+    LOGD (MODULE, "Empty the data buffer halfway");
+    for (uint8_t dataNum = ZERO; dataNum < CircBufferLen * 0.5; dataNum++)
+    {
+        CircBuffer.Remove ();
+    }
+
+    LOGD (MODULE, "Check the contents of the buffer");
+    for (uint8_t dataNum = ZERO; dataNum < CircBufferLen; dataNum++)
+    {
+        if (dataNum < CircBufferLen * 0.5)
+        {
+            ASSERT_EQ (dataNum + 5, CircBuffer.Remove ());
+        }
+        else
+        {
+            ASSERT_EQ (ZERO, CircBuffer.Remove ());
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////// END OF FILE ///////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
